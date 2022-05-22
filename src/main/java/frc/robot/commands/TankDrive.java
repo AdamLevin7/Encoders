ad10cd0fce0df13d73045331e3a27859249593ca
AdamLevin7 <63608754+AdamLevin7@users.noreply.github.com>
2022-05-22 16:11:40 -0700

    first commit

---
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -0,0 +1,46 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveTrain;
+
+public class TankDrive extends CommandBase {
+
+  private final DriveTrain _driveTrain;
+  private final Joystick _leftJoystick;
+  private final Joystick _rightJoystick;
+  
+  /** Creates a new TankDrive. */
+  public TankDrive(DriveTrain dt, Joystick lj, Joystick rj) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    _driveTrain = dt;
+    _leftJoystick = lj;
+    _rightJoystick = rj;
+
+    
+    addRequirements(_driveTrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    _driveTrain.tankDrive(-0.8 * _leftJoystick.getRawAxis(Constants.JoystickAxis.YAxis),
+                          -0.8 * _rightJoystick.getRawAxis(Constants.JoystickAxis.YAxis));
+                          
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+  
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,54 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class DriveTrain extends SubsystemBase {
+
+  private final WPI_TalonSRX _leftDriveTalon;
+  private final WPI_TalonSRX _righttDriveTalon;
+
+  private DifferentialDrive _diffDrive;
+
+
+  /** Creates a new DriveTrain. */
+  public DriveTrain() {
+    _leftDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.LeftDriveTalonPort);
+    _righttDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.RightDriveTalonPort);
+
+    _leftDriveTalon.setInverted(false);
+    _righttDriveTalon.setInverted(false);
+    
+
+    _diffDrive = new DifferentialDrive(_leftDriveTalon, _righttDriveTalon);
+
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Position", getPos());
+  }
+
+  public void tankDrive(double leftSpeed, double rightSpeed) {
+    _diffDrive.tankDrive(leftSpeed, rightSpeed);
+  }
+  public void arcadeDrive(double speed, double rotation) {
+    _diffDrive.tankDrive(speed, rotation);
+  }
+  public double getPos(){
+    return _leftDriveTalon.getSelectedSensorPosition();
+  }
+  public void setPos(double pos){
+    _leftDriveTalon.setSelectedSensorPosition(pos);
+  }
+}
